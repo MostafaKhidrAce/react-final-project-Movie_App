cd86d0e356bda04866e6e5970a338d26ca428cbf
Mostafa Khidr <mostafa5idr.ace@gmail.com>
2023-10-25 23:47:24 +0300

    filtration locally

--- src/components/pages-page/PagesDisplay.jsx
+++ src/components/pages-page/PagesDisplay.jsx
@@ -4,6 +4,7 @@ import Loading from "../Loading/Loading";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 import { useState, useEffect } from "react";
+import "./PagesDisplay.css";
 
 const PagesDisplay = () => {
   const [page, setPage] = useState(1);
@@ -13,11 +14,21 @@ const PagesDisplay = () => {
     setPage(value);
   };
 
+  const [mediaTypeFilter, setMediaTypeFilter] = useState("all");
+
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   }, [page]);
 
-  const movieList = data.map((movie) => {
+  const filteredData = data.filter((item) => {
+    if (mediaTypeFilter === "all") {
+      return true;
+    } else {
+      return item.media_type === mediaTypeFilter;
+    }
+  });
+
+  const movieList = filteredData.map((movie) => {
     return (
       <div key={movie.id} className="col-lg-3 col-md-4 col-sm-6 ">
         <MyCard movie={movie} />
@@ -29,6 +40,21 @@ const PagesDisplay = () => {
     <>
       <div className="container">
         <h2 className="trending_section_title">Latest Movies & Tv Shows</h2>
+        <div className="filter-container">
+          <label htmlFor="mediaTypeFilter" className="filter-label">
+            Filter by Media Type:
+          </label>
+          <select
+            id="mediaTypeFilter"
+            value={mediaTypeFilter}
+            onChange={(event) => setMediaTypeFilter(event.target.value)}
+            className="filter-select"
+          >
+            <option value="all">All</option>
+            <option value="movie">Movies</option>
+            <option value="tv">TV Shows</option>
+          </select>
+        </div>
         <Loading loading={loading} error={error}>
           <div className="row">{movieList}</div>
         </Loading>
